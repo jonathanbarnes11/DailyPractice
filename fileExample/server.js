4e148a072004aac4ddf577a2539a5719e90c9c6e
jbarnes <jbarnes@appnexus.com>
2017-01-18 15:15:26 -0800

    Practice for the day got environment configured hopefully practice will be faster and easier now

--- fileExample/server.js
+++ fileExample/server.js
@@ -1,16 +1,24 @@
-var http = require("http");
-var url = require("url");
+var http = require('http');
+var url = require('url');
 
-function start() {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request received for " + pathname);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
+function start(route, handle) {
+	function onRequest(request, response) {
+		var postData = '';
+		var pathname = url.parse(request.url).pathname;
+		console.log('Request received for ' + pathname);
 
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+		request.setEncoding('utf8');
+
+		request.addListener('data', function(postDataChunk) {
+			postData += postDataChunk;
+			console.log('Recieved Post data chunk' + postDataChunk);
+		});
+		request.addListener('end', function() {
+			route(handle, pathname, response);
+		});
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
 exports.start = start;
