edd109226f690934ce9f0c5ba685cccf206eb09f
jbarnes <jbarnes@appnexus.com>
2017-01-21 10:43:49 -0800

    More practice

--- fileExample/requestHandlers.js
+++ fileExample/requestHandlers.js
@@ -1,30 +1,31 @@
-var exec = require("child_process").exec;
+var exec = require('child_process').exec;
+var querystring = require('querystring');
 
-function start(response) {
-  var body = '<html>'+
- '<head>'+
- '<meta http-equiv="Content-Type" content="text/html; '+
- 'charset=UTF-8" />'+
- '</head>'+
- '<body>'+
- '<form action="/upload" method="post">'+
- '<textarea name="text" rows="20" cols="60"></textarea>'+
- '<input type="submit" value="Submit text" />'+
- '</form>'+
- '</body>'+
- '</html>';
+function start(response, postData) {
+	var body = '<html>'+
+	'<head>'+
+	'<meta http-equiv="Content-Type" content="text/html; '+
+	'charset=UTF-8" />'+
+	'</head>'+
+	'<body>'+
+	'<form action="/upload" method="post">'+
+	'<textarea name="text" rows="20" cols="60"></textarea>'+
+	'<input type="submit" value="Submit text" />'+
+	'</form>'+
+	'</body>'+
+	'</html>';
 
- console.log("Request handler 'start' was called")
-// exec("", function (error, stdout, stderr) { response.writeHead(200, {"Content-Type": "text/plain"}); response.write(stdout););
- response.writeHead(200, {"Content-Type" : "text/html"});
- response.write(body);
- response.end();
+	//console.log('Request handler 'start' was called')
+	// exec("", function (error, stdout, stderr) { response.writeHead(200, {"Content-Type": "text/plain"}); response.write(stdout););
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write(body);
+	response.end();
 }
 
-function upload(response) {
- console.log("Request handler 'upload' was called");
- response.writeHead(200, {"Content-Type": "text/plain"}); response.write("Hello Upload");
- response.end();
+function upload(response, postData) {
+	//console.log("Request handler 'upload' was called");
+	response.writeHead(200, {'Content-Type': 'text/plain'}); response.write('Message:' + postData);
+	response.end();
 }
 
 exports.start = start;
--- fileExample/server.js
+++ fileExample/server.js
@@ -14,11 +14,11 @@ function start(route, handle) {
 			console.log('Recieved Post data chunk' + postDataChunk);
 		});
 		request.addListener('end', function() {
-			route(handle, pathname, response);
+			route(handle, pathname, response, postData);
 		});
 	}
 
 	http.createServer(onRequest).listen(8888);
-	console.log("Server has started.");
+	console.log('Server has started.');
 }
 exports.start = start;
